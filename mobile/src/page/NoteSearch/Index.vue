529c9879bd5b38ffa1f63431f599466ec9592ab8
zhangjie <58214381@qq.com>
2017-09-21 10:35:22 +0800

    月+1

--- mobile/src/page/NoteSearch/Index.vue
+++ mobile/src/page/NoteSearch/Index.vue
@@ -70,7 +70,7 @@ export default {
 
 					 let date = new Date(item.updateTime*1000);
 
-					 item.updateTime = `${date.getFullYear()}-${this.webApi.isSmallTen(date.getMonth())}-${this.webApi.isSmallTen(date.getDate())}  ${this.webApi.isSmallTen(date.getHours())}:${this.webApi.isSmallTen(date.getMinutes())}`;
+					 item.updateTime = `${date.getFullYear()}-${this.webApi.isSmallTen(date.getMonth() + 1)}-${this.webApi.isSmallTen(date.getDate())}  ${this.webApi.isSmallTen(date.getHours())}:${this.webApi.isSmallTen(date.getMinutes())}`;
 
 				 });
 
